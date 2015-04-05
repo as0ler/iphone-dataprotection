348dc0cd13bd57ad88401838ec6857452ad1d315
as0ler <dark.cancerbero@gmail.com>
2015-04-05 18:22:16 +0200

    add all new files

--- python_scripts/keychain/__init__.py
+++ python_scripts/keychain/__init__.py
@@ -4,9 +4,9 @@ from keychain4 import Keychain4
 
 def keychain_load(filename, keybag, key835):
     version = sqlite3.connect(filename).execute("SELECT version FROM tversion").fetchone()[0]
-    #print "Keychain version : %d" % version
+    print "Keychain version : %d" % version
     if version == 3:
         return Keychain3(filename, key835)
     elif version >= 4:
-        return Keychain4(filename, keybag)
+    	return Keychain4(filename, keybag)
     raise Exception("Unknown keychain version %d" % version)
